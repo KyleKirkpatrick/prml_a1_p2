207680b964b230909cc359d6a9468ee29f83d69a
Kyle Kirkpatrick <kylebkirkpatrick@live.com>
2026-09-12 22:51:52 +1000

    explicitly define random_state and penalty

--- main.py
+++ main.py
@@ -74,9 +74,11 @@ x_test_flat = x_test_flat.astype('float32') / 255.0
 
 # Using 'saga' solver for large datasets and multinomial classification
 model = LogisticRegression(
+    penalty='l2',       # Apply L2 regularisation to the model weights
     solver='saga',
     # multi_class='multinomial',    # 'multinomial' is the default for 'saga' solver, so this line can be omitted
     max_iter=100,      # Number of iterations
+    random_state=42,   # Make the stochastic solver reproducible
     verbose=1,         # Show training progress
     # n_jobs=-1          # deprecated
 )
